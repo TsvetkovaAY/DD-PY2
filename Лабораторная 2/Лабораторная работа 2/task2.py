70bbbbbc91900574672aa0a7891e4c1ea8dd7c34
TsvetkovaAY <115409533+TsvetkovaAY@users.noreply.github.com>
2023-01-31 15:19:06 +0300

    Update task2.py

--- Лабораторная 2/Лабораторная работа 2/task2.py
+++ Лабораторная 2/Лабораторная работа 2/task2.py
@@ -1,11 +1,11 @@
 BOOKS_DATABASE = [
     {
-        "id": 1,
+        "id_": 1,
         "name": "test_name_1",
         "pages": 200,
     },
     {
-        "id": 2,
+        "id_": 2,
         "name": "test_name_2",
         "pages": 400,
     }
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id_"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
